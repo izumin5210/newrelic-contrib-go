eefe3d17dec6f6b7bbf4c12d8e6d243b8c86c7dd
izumin5210 <m@izum.in>
2018-05-02 14:51:19 +0900

    Add util for set/get transaction into/from context

---
+++ nrutil/ctx.go
@@ -0,0 +1,29 @@
+package nrutil
+
+import (
+	"github.com/newrelic/go-agent"
+	"golang.org/x/net/context"
+)
+
+type key struct{}
+
+var (
+	txnKey key
+)
+
+// Transaction extracts newrelic transaction object from request context
+func Transaction(ctx context.Context) newrelic.Transaction {
+	v := ctx.Value(txnKey)
+	if v == nil {
+		return nil
+	}
+	if txn, ok := v.(newrelic.Transaction); ok {
+		return txn
+	}
+	return nil
+}
+
+// SetTransaction stores newrelic transaction object into given context.
+func SetTransaction(ctx context.Context, txn newrelic.Transaction) context.Context {
+	return context.WithValue(ctx, txnKey, txn)
+}
